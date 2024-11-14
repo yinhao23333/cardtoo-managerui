b121ed91c8a58af2305da24b2742d8352d57336c
yinhao <13017196293@163.com>
2024-11-14 14:20:20 +0800

    去除若依默认首页

--- ruoyi-ui/src/router/index.js
+++ ruoyi-ui/src/router/index.js
@@ -62,19 +62,6 @@ export const constantRoutes = [
     hidden: true
   },
   {
-    path: '',
-    component: Layout,
-    redirect: 'index',
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/index'),
-        name: 'Index',
-        meta: { title: '首页', icon: 'dashboard', affix: true }
-      }
-    ]
-  },
-  {
     path: '/user',
     component: Layout,
     hidden: true,
